e00a6994bcbd544048cbe48b0c253db33aeff155
marco.buchholz@bfh.ch <marco.buchholz@bfh.ch>
2023-03-09 14:04:21 +0100

    shepherds hut

--- src/app/pages/shop/checkout/checkout.component.ts
+++ src/app/pages/shop/checkout/checkout.component.ts
@@ -3,171 +3,175 @@ import { Subscription } from 'rxjs';
 
 import { CartService } from 'src/app/shared/services/cart.service';
 import { environment } from 'src/environments/environment';
-import {loadStripe} from '@stripe/stripe-js'
+import { loadStripe } from '@stripe/stripe-js';
 import { async } from 'rxjs/internal/scheduler/async';
-import { NgbCalendar, NgbDate, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbCalendar,
+  NgbDate,
+  NgbDateStruct,
+} from '@ng-bootstrap/ng-bootstrap';
 import { FormControl } from '@angular/forms';
 import { WeekDay } from '@angular/common';
 
-const stripePromise = loadStripe(environment.STRIPE_SK)
+const stripePromise = 'test';
 
 declare var $: any;
 
 @Component({
-	selector: 'shop-checkout-page',
-	templateUrl: './checkout.component.html',
-	styleUrls: ['./checkout.component.scss']
+  selector: 'shop-checkout-page',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.scss'],
 })
-
 export class CheckoutComponent implements OnInit, OnDestroy {
+  telephone = '';
 
-	telephone = ""
+  errorMessage = 'Please fill in the whole form including date and time';
 
-	errorMessage = "Please fill in the whole form including date and time"
+  radio: FormControl;
 
-	radio: FormControl
+  test: '';
 
-	test: "" 
+  markDisabled = (date: NgbDate) => this.getDay(date);
 
-	markDisabled = (date: NgbDate) =>  this.getDay(date)
+  minDate = { day: 0, year: 0, month: 0 };
 
-	minDate = {day: 0, year: 0, month: 0}
+  selfPickup = true;
 
+  storePickup = false;
 
-	selfPickup = true
-	
-	storePickup = false
+  model: NgbDateStruct;
 
-	model: NgbDateStruct;
+  valid = false;
 
-	valid = false
+  date: { year: number; month: number };
 
-	date: {year: number, month: number}
+  info = {
+    telephone: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+  };
 
-	info = {
-		telephone: "",
-		firstName: "",
-		lastName: "",
-		email: "",
-	}
+  getDay(date) {
+    const disabled = [1, 2, 3, 7];
+    console.log(this.minDate);
+    if (disabled.includes(this.calendar.getWeekday(date))) return true;
+    return false;
+  }
 
-	getDay(date){
-		const disabled = [1,2, 3,7]
-		console.log(this.minDate);
-		if(disabled.includes(this.calendar.getWeekday(date))) return true
-		return false
-	}
+  checkValidity() {
+    let date = false;
+    let firstname = false;
+    let lastname = false;
+    let phone = false;
+    let email = false;
+    if (this.info.firstName) {
+      firstname = true;
+    }
+    if (this.info.lastName) {
+      lastname = true;
+    }
+    if (this.info.telephone) {
+      phone = true;
+    }
+    if (this.info.email) {
+      email = true;
+    }
+    if (this.model) date = true;
+    if (date && firstname && lastname && email && phone) {
+      this.valid = true;
+      return false;
+    }
+    // console.log("getting to here");
+    return true;
+  }
 
-	checkValidity(){
-		let date = false
-		let firstname = false
-		let lastname = false
-		let phone = false
-		let email = false
-		if(this.info.firstName){
-			firstname = true
-		}
-		if(this.info.lastName){
-			lastname = true
-		}
-		if(this.info.telephone){
-			phone = true
-		}
-		if(this.info.email){
-			email = true
-		}
-		if(this.model) date = true
-		if(date && firstname && lastname && email && phone){
-			this.valid = true;
-			return false
-		}
-		// console.log("getting to here");
-		return true
-	}
+  cartItems = [];
 
+  private subscr: Subscription;
 
-	cartItems = [];
+  constructor(public cartService: CartService, public calendar: NgbCalendar) {
+    let date = new Date();
+    this.minDate.year = date.getFullYear();
+    this.minDate.month = date.getMonth() + 1;
+    this.minDate.day = date.getDate() + 1;
+    console.log(this.minDate);
+  }
 
-	private subscr: Subscription;
+  ngOnInit(): void {
+    this.subscr = this.cartService.cartStream.subscribe((items) => {
+      this.cartItems = items;
+    });
+    document
+      .querySelector('body')
+      .addEventListener('click', () => this.clearOpacity());
+    console.log(this.cartItems, 'cartitems');
+  }
 
-	constructor(public cartService: CartService, public calendar: NgbCalendar) {
-		let date = new Date()
-		this.minDate.year = date.getFullYear()
-		this.minDate.month = date.getMonth() + 1
-		this.minDate.day = date.getDate() + 1
-		console.log(this.minDate);
-	}
+  changeValue() {
+    this.selfPickup = !this.selfPickup;
+  }
 
-	ngOnInit(): void {
+  handleBuy = async () => {
+    console.log(this.model, 'model');
+    const obj = {
+      user: this.info,
+      product: [],
+      date: this.model,
+      selfPickup: this.selfPickup,
+    };
+    for (const product of this.cartItems) {
+      console.log(product, 'product');
+      obj.product.push({ id: product.slug, qty: product.qty });
+    }
+    const stripe = await stripePromise;
+    const res = await fetch(`${environment.SERVER_URL}/api/orders`, {
+      method: 'POST',
+      body: JSON.stringify(obj),
+    });
+    const session = await res.json();
+  };
 
-		this.subscr = this.cartService.cartStream.subscribe(items => {
-			this.cartItems = items;
-		});
-		document.querySelector('body').addEventListener("click", () => this.clearOpacity())
-		console.log(this.cartItems, "cartitems");
-	}
+  ngOnDestroy(): void {
+    this.subscr.unsubscribe();
+    document
+      .querySelector('body')
+      .removeEventListener('click', () => this.clearOpacity());
+  }
 
-	changeValue(){
-		this.selfPickup = !this.selfPickup
-	}
+  clearOpacity() {
+    let input: any = document.querySelector('#checkout-discount-input');
+    if (input && input.value == '') {
+      let label: any = document.querySelector('#checkout-discount-form label');
+      label.removeAttribute('style');
+    }
+  }
 
-	handleBuy = async () => {
-		console.log(this.model, "model");
-		const obj = {user: this.info, product: [], date: this.model, selfPickup: this.selfPickup}
-		for(const product of this.cartItems){
-			console.log(product, "product");
-			obj.product.push({id: product.slug, qty: product.qty})
-		}
-		const stripe = await stripePromise
-		const res = await fetch(`${environment.SERVER_URL}/api/orders`, {
-			method: 'POST',
-			body: JSON.stringify(obj)
-		})
-		const session = await res.json()
+  addOpacity(event: any) {
+    event.target.parentElement
+      .querySelector('label')
+      .setAttribute('style', 'opacity: 0');
+    event.stopPropagation();
+  }
 
-		const result = await stripe.redirectToCheckout({
-			sessionId: session.id
-		})
-	}
+  formToggle(event: any) {
+    console.log('getting executed');
+    const parent: HTMLElement = event.target.closest('.custom-control');
+    const submenu: HTMLElement = parent
+      .closest('.form-group')
+      .querySelector('.shipping-info');
 
+    if (parent.classList.contains('open')) {
+      $(submenu).slideUp(300, function () {
+        parent.classList.remove('open');
+      });
+    } else {
+      $(submenu).slideDown(300, function () {
+        parent.classList.add('open');
+      });
+    }
 
-	ngOnDestroy(): void {
-		this.subscr.unsubscribe();
-		document.querySelector('body').removeEventListener("click", () => this.clearOpacity())
-	}
-
-	clearOpacity() {
-		let input: any = document.querySelector('#checkout-discount-input');
-		if (input && input.value == "") {
-			let label: any = document.querySelector('#checkout-discount-form label');
-			label.removeAttribute('style');
-		}
-	}
-
-	addOpacity(event: any) {
-		event.target.parentElement.querySelector("label").setAttribute("style", "opacity: 0");
-		event.stopPropagation();
-
-	}
-
-
-	formToggle(event: any) {
-		console.log("getting executed");
-		const parent: HTMLElement = event.target.closest('.custom-control');
-		const submenu: HTMLElement = parent.closest('.form-group').querySelector('.shipping-info');
-
-		if (parent.classList.contains('open')) {
-			$(submenu).slideUp(300, function () {
-				parent.classList.remove('open');
-			});
-		}
-		else {
-			$(submenu).slideDown(300, function () {
-				parent.classList.add('open');
-			});
-		}
-
-		event.preventDefault();
-		event.stopPropagation();
-	}
+    event.preventDefault();
+    event.stopPropagation();
+  }
 }
--- src/app/shepherd/shepherd.component.ts
+++ src/app/shepherd/shepherd.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  customSay,
+  team,
+  brands,
+} from '../pages/others/about-one/about-one-data';
+import { sliderOpt } from 'src/app/shared/data';
 @Component({
   selector: 'molla-shepherd',
   templateUrl: './shepherd.component.html',
-  styleUrls: ['./shepherd.component.scss']
+  styleUrls: ['./shepherd.component.scss'],
 })
 export class ShepherdComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  customSay = customSay;
+  brands = brands;
+  team = team;
+  sliderOption = {
+    ...sliderOpt,
+    nav: false,
+    dots: true,
+    margin: 20,
+    loop: false,
+    responsive: {
+      1200: {
+        nav: true,
+      },
+    },
+  };
+  teamOption = {
+    ...sliderOpt,
+    nav: false,
+    dots: false,
+    items: 3,
+    margin: 20,
+    loop: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  };
 }
